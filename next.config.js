1f510afe38ec01ec444178e150b90e1677972e9a
alphineaiinc <alphineai.inc@gmail.com>
2025-10-04 08:40:28 -0400

    Cleaned Next.js config for v15 (removed deprecated appDir)

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
-/** @type {import('next').NextConfig} */ 
-const nextConfig = {reactStrictMode:true,experimental:{appDir:true},}; 
-module.exports = nextConfig; 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // App Router is automatically enabled in Next.js 15; no need for experimental.appDir
+};
+
+module.exports = nextConfig;
